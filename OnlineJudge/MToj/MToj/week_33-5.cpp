58e8062b8d265bb1f769b084c891cc786dda11d8
Crestimes <1259864733@qq.com>
2023-11-23 15:50:43 +0800

    求和(双指针)

---
+++ OnlineJudge/MToj/MToj/week_33-5.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+using namespace std;
+
+int main() {
+	int l = 1, r = 2, x, sum = 0, flag = 0;
+	cin >> x;
+	while (l <= x / 2) {
+		sum = (l + r) * (r - l + 1) / 2;
+		if (sum == x) {
+			flag = 1;
+			cout << l << " " << r << endl;
+			l++, r++;
+		}
+		else if (sum > x) l++;
+		else r++;
+	}
+	if (!flag)cout << "No" << endl;
+	return 0;
+}
