7f186b213ff0c2c098edb328022a5707687f4cb2
Crestimes <1259864733@qq.com>
2023-11-23 16:26:01 +0800

    战神的对称谜题(双指针)

---
+++ OnlineJudge/MToj/MToj/week_33-1.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+	string s;
+	getline(cin, s);
+	int ans = 0, l, r, temp;
+	for (int i = 0; i < s.length(); i++) {
+		l = i - 1;
+		r = i + 1;
+		temp = 1;
+		while (l >= 0 && r < s.length() && s[l] == s[r]) {
+			temp += 2;
+			l--, r++;
+		}
+		ans = max(ans, temp);
+		l = i;
+		r = i + 1;
+		temp = 0;
+		while (l >= 0 && r < s.length() && s[l] == s[r]) {
+			temp += 2;
+			l--, r++;
+		}
+		ans = max(ans, temp);
+	}
+	cout << ans;
+	return 0;
+}
