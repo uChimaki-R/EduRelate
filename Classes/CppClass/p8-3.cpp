e0712d7b03d84ff01393acbb73e057ef7d0ca01e
Crestimes <1259864733@qq.com>
2023-12-06 10:57:02 +0800

    数据结构作业8.3、8.4

---
+++ Classes/CppClass/p8-3.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+typedef pair<int, int> pii;
+
+vector<int> dijkstra(const vector<vector<pii>>& graph, int start) {
+	int n = graph.size();
+	vector<int> dist(n, INT_MAX);
+	vector<bool> visited(n, false);
+	priority_queue<pii, vector<pii>, greater<pii>> pq;
+
+	dist[start] = 0;
+	pq.push({ 0, start });
+
+	while (!pq.empty()) {
+		int u = pq.top().second;
+		pq.pop();
+
+		if (visited[u]) continue;
+		visited[u] = true;
+
+		for (const auto& edge : graph[u]) {
+			int v = edge.first;
+			int w = edge.second;
+
+			if (!visited[v] && dist[u] + w < dist[v]) {
+				dist[v] = dist[u] + w;
+				pq.push({ dist[v], v });
+			}
+		}
+	}
+
+	return dist;
+}
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+
+	vector<vector<pii>> graph(n + 1);
+	for (int i = 0; i < m; ++i) {
+		int u, v, w;
+		cin >> u >> v >> w;
+		graph[u].push_back({ v, w });
+		graph[v].push_back({ u, w });
+	}
+
+	int start;
+	cin >> start;
+
+	vector<int> dist = dijkstra(graph, start);
+
+	for (int i = 1; i <= n; ++i) {
+		cout << "从顶点 " << start << " 到顶点 " << i << " 的最短路径长度为： " << dist[i] << endl;
+	}
+
+	return 0;
+}
---
+++ Classes/CppClass/p8-4.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+void topologicalSort(vector<vector<int>>& graph, vector<int>& result) {
+	int n = graph.size();
+	vector<int> inDegree(n, 0);
+
+	for (int i = 1; i < n; ++i) {
+		for (int j = 0; j < graph[i].size(); ++j) {
+			++inDegree[graph[i][j]];
+		}
+	}
+
+	queue<int> q;
+	for (int i = 1; i < n; ++i) {
+		if (inDegree[i] == 0) {
+			q.push(i);
+		}
+	}
+
+	while (!q.empty()) {
+		int u = q.front();
+		q.pop();
+		result.push_back(u);
+
+		for (int v : graph[u]) {
+			--inDegree[v];
+			if (inDegree[v] == 0) {
+				q.push(v);
+			}
+		}
+	}
+}
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+	vector<vector<int>> graph(n + 1);
+	for (int i = 0; i < m; ++i) {
+		int u, v;
+		cin >> u >> v;
+		graph[u].push_back(v);
+	}
+
+	vector<int> result;
+	topologicalSort(graph, result);
+
+	for (int u : result) {
+		cout << u << " ";
+	}
+	cout << endl;
+
+	return 0;
+}
