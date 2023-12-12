1bc7fd0ee3f593cf8e19bf21eeae7f3ab9171973
Crestimes <1259864733@qq.com>
2023-12-12 23:53:01 +0800

    python实验5 练习1

---
+++ Classes/PythonClass/exps/work_5/exp-1.py
@@ -0,0 +1,38 @@
+import jieba
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+from imageio.v2 import imread
+import os
+
+
+path = "testwordcloud/"
+
+with open(os.path.join(path, '2020_report.txt'), 'r', encoding='utf-8') as f:
+    data = f.read()
+sentences = data.split('#')
+
+with open(os.path.join(path, 'stopword.txt'), 'r', encoding='utf-8') as f:
+    stopwords = f.readlines()
+stopwords = [word.strip() for word in stopwords]
+stopwords.append('\n')
+
+mask = imread(os.path.join(path, 'chinamap.png'))
+
+for i, sentence in enumerate(sentences):
+    words = list(jieba.cut(sentence))
+    words_dict = {}
+
+    for word in words:
+        if word in stopwords:
+            words.remove(word)
+        else:
+            words_dict[word] = words_dict.get(word, 0) + 1
+
+    wc = WordCloud(font_path=os.path.join(path, 'simhei.ttf'), background_color='white', mask=mask)
+    wc.generate_from_frequencies(words_dict)
+
+    plt.subplot(2, 4, i+1)
+    plt.axis('off')
+    plt.imshow(wc)
+
+plt.show()
