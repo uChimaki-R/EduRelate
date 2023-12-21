2582cfb5480ef0c3b93fa7dd5a9849cc946a7133
Crestimes <1259864733@qq.com>
2023-12-21 23:57:24 +0800

    数据结构实验7

---
+++ Classes/DataStructure/data_7/data_7/work-1.cpp
@@ -0,0 +1,52 @@
+﻿//#include <iostream>
+//#include <algorithm>
+//using namespace std;
+//
+//int binary_search(int arr[], int n, int target) {
+//	int left = 0;
+//	int right = n - 1;
+//
+//	while (left <= right) {
+//		int mid = left + (right - left) / 2;
+//
+//		if (arr[mid] == target) {
+//			return mid;
+//		}
+//		else if (arr[mid] < target) {
+//			left = mid + 1;
+//		}
+//		else {
+//			right = mid - 1;
+//		}
+//	}
+//
+//	return -1;
+//}
+//
+//int main() {
+//	int n, target;
+//	int* arr = new int[10000];
+//
+//	cout << "请输入数组元素个数：";
+//	cin >> n;
+//
+//	cout << "请输入数组元素：";
+//	for (int i = 0; i < n; i++) {
+//		cin >> arr[i];
+//	}
+//
+//	cout << "请输入需要搜索的数字：";
+//	cin >> target;
+//
+//	int result = binary_search(arr, n, target);
+//
+//	if (result != -1) {
+//		cout << "元素 " << target << " 在数组中的索引为： " << result << endl;
+//	}
+//	else {
+//		cout << "元素 " << target << " 不在数组中" << endl;
+//	}
+//
+//	delete[]arr;
+//	return 0;
+//}
---
+++ Classes/DataStructure/data_7/data_7/work-2.cpp
@@ -0,0 +1,77 @@
+﻿#include <iostream>
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode* left = nullptr;
+	TreeNode* right = nullptr;
+	TreeNode(int x) : val(x) {}
+};
+
+class BST {
+public:
+	// 查找元素，如果找不到则插入到二叉搜索树中
+	void searchOrInsert(int val) {
+		TreeNode* node = find(root, val);
+		if (node == NULL) {
+			node = new TreeNode(val);
+			insert(root, node);
+			cout << "<Insert Node> val: " << node->val << endl;
+		}
+		else {
+			cout << "<Existing Node> val: " << node->val << endl;
+		}
+	}
+
+private:
+	TreeNode* root = nullptr;
+
+	// 查找元素
+	TreeNode* find(TreeNode* node, int val) {
+		if (node == NULL || node->val == val) {
+			return node;
+		}
+		else if (node->val > val) {
+			return find(node->left, val);
+		}
+		else {
+			return find(node->right, val);
+		}
+	}
+
+	// 插入元素
+	void insert(TreeNode*& node, TreeNode* newNode) {
+		if (node == NULL) {
+			node = newNode;
+		}
+		else if (newNode->val < node->val) {
+			insert(node->left, newNode);
+		}
+		else {
+			insert(node->right, newNode);
+		}
+	}
+};
+
+int main() {
+	BST bst;
+
+	/*bst.searchOrInsert(1);
+	bst.searchOrInsert(2);
+	bst.searchOrInsert(3);
+	bst.searchOrInsert(3);
+	bst.searchOrInsert(2);
+	bst.searchOrInsert(7);
+	bst.searchOrInsert(8);
+	bst.searchOrInsert(1);*/
+
+	while (true) {
+		int val;
+		cout << "searchOrInsert(0 to exit): ";
+		cin >> val;
+		if (!val)break;
+		bst.searchOrInsert(val);
+	}
+
+	return 0;
+}
