8c1204f825faabafb6c351cb24f4784b63a1f391
Crestimes <1259864733@qq.com>
2024-03-01 18:45:26 +0800

    测试文件

---
+++ Classes/CompilationPrinciple/CppWordAssembler/CppWordAssembler/my_test.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a = 4, b = 6;
+	long c;
+	float d = 5.789;
+	string e = "hahaha"; //I say something
+	/*float f = 7.342;*/
+	a += b;
+	if (a == b) {
+		/*cin>>a;
+		cin>>b;*/
+		cout << "hahahahaha";
+	}
+	return 0;
+}
