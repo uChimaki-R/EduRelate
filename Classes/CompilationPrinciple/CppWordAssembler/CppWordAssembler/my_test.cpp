bfcd50d268967b94cc9d2120f910fdde03160b68
Crestimes <1259864733@qq.com>
2024-03-22 15:20:23 +0800

    添加对应功能的测试代码

--- Classes/CompilationPrinciple/CppWordAssembler/CppWordAssembler/my_test.cpp
+++ Classes/CompilationPrinciple/CppWordAssembler/CppWordAssembler/my_test.cpp
@@ -10,9 +10,14 @@ int main()
 	int a = 4, b = -6;
 	long c;
 	float d = -5.789;
+	d = 3.455e+5;
+	d = 4.66E9;
+	c = 0x333;
+	c = 0X787;
 	string e = "hahaha"; //I say something
 	/*float f = 7.342;*/
 	a += b;
+	char tt = 'c';
 	if (a == b) {
 		/*cin>>a;
 		cin>>b;*/
