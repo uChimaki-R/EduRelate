fe16be7f64d8e7c6cee4536f26516d9214f05e5a
Crestimes <1259864733@qq.com>
2024-03-24 14:34:33 +0800

    模拟题组星耀难度

---
+++ OnlineJudge/MToj/MTPhase3/MTPhase3/week_8-3-sta.cpp
@@ -0,0 +1,54 @@
+﻿#include<iostream>
+#include<string.h>
+using namespace std;
+
+const int N = 107;
+int arr[N][N];
+int temp[N][N];
+int n, m, sum = 0;
+
+bool check(int l, int w) {
+	for (int i = 1; i <= n - l + 1; i++) {
+		for (int j = 1; j <= m - w + 1; j++) {
+			// 左上角的点
+			int num = temp[i][j];
+			// 对刻章内部的点都-num
+			for (int k = 0; k < l; k++) {
+				for (int p = 0; p < w; p++) {
+					int ii = i + k, jj = j + p;
+					temp[ii][jj] -= num;
+					if (temp[ii][jj] < 0)return 0;
+				}
+			}
+		}
+	}
+	// 检查是否全部为0
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			if (temp[i][j])return 0;
+		}
+	}
+	return 1;
+}
+
+int main() {
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= m; j++) {
+			cin >> arr[i][j];
+			sum += arr[i][j];
+		}
+	}
+	for (int l = n; l >= 1; l--) {
+		for (int w = m; w >= 1; w--) {
+			// 刻章大小
+			if (sum % (l * w))continue;
+			memcpy(temp, arr, sizeof(arr));
+			if (check(l, w)) {
+				cout << sum / (l * w);
+				return 0;
+			}
+		}
+	}
+	return 0;
+}
---
+++ OnlineJudge/MToj/MTPhase3/MTPhase3/week_8-5-sta.cpp
@@ -0,0 +1,49 @@
+﻿#include<iostream>
+#include<stack>
+#include<vector>
+using namespace std;
+
+const int N = 200007;
+
+int books[N];
+stack<int>st;
+vector<int>ans;
+
+int main() {
+	int n, c;
+	cin >> n >> c;
+	for (int i = n; i >= 1; i--)cin >> books[i];
+	books[0] = n;
+	while (true) {
+		if (books[0] == 0) {
+			while (!st.empty()) {
+				ans.push_back(st.top());
+				st.pop();
+			}
+			break;
+		}
+		int st_size = c - st.size();
+		int cmp_cnt = min(books[0], st_size);
+		int min_num = N;
+		int st_top = N;
+		if (!st.empty())st_top = st.top();
+		for (int i = books[0]; cmp_cnt--; i--) {
+			if (books[i] < min_num) {
+				min_num = books[i];
+			}
+		}
+		if (min_num < st_top) {
+			for (int i = books[0]; ; i--) {
+				st.push(books[i]);
+				books[0]--;
+				if (books[i] == min_num)break;
+			}
+		}
+		ans.push_back(st.top());
+		st.pop();
+	}
+	for (auto i : ans) {
+		cout << i << " ";
+	}
+	return 0;
+}
