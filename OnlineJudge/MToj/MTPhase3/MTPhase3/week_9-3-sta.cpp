016ec730336c682a55d63cfebcdd018511976401
Crestimes <1259864733@qq.com>
2024-03-31 15:25:05 +0800

    第九周模拟题组星耀难度

---
+++ OnlineJudge/MToj/MTPhase3/MTPhase3/week_9-3-sta.cpp
@@ -0,0 +1,40 @@
+﻿#include<iostream>
+using namespace std;
+
+int n, a, b, c;
+
+bool check(int w, int h) {
+	if (w >= 3 * a + b + c && h >= b + c)return true;
+	if (w >= 2 * a + 2 * b && h >= 2 * a + c)return true;
+	if (w >= 2 * a + 2 * b && h >= a + b + c)return true;
+	if (w >= a + b + 2 * c && h >= a + b + c)return true;
+	return false;
+}
+
+int main() {
+	cin >> n >> a >> b >> c;
+	int ans = 0;
+	while (n--) {
+		int w, h;
+		cin >> w >> h;
+		if (check(w, h)) { ans++; continue; }
+		if (check(h, w)) { ans++; continue; }
+		swap(a, b);
+		if (check(w, h)) { ans++; continue; }
+		if (check(h, w)) { ans++; continue; }
+		swap(b, c);
+		if (check(w, h)) { ans++; continue; }
+		if (check(h, w)) { ans++; continue; }
+		swap(a, c);
+		if (check(w, h)) { ans++; continue; }
+		if (check(h, w)) { ans++; continue; }
+		swap(a, b);
+		if (check(w, h)) { ans++; continue; }
+		if (check(h, w)) { ans++; continue; }
+		swap(b, c);
+		if (check(w, h)) { ans++; continue; }
+		if (check(h, w)) { ans++; continue; }
+	}
+	cout << ans << endl;
+	return 0;
+}
---
+++ OnlineJudge/MToj/MTPhase3/MTPhase3/week_9-4-sta.cpp
@@ -0,0 +1,37 @@
+﻿#include<iostream>
+using namespace std;
+
+int n;
+int num[5] = { 0, 2, 6, 10, 14 };
+int sum[10][5];
+string s = " spdf";
+
+void fun(int x, int i, int j) {
+	sum[i][j] = min(x, num[j]);
+	int y = x - num[j];
+	if (y <= 0)return;
+	if (j == 1) {
+		j = i / 2 + 1;
+		i = (i + 1) / 2 + 1;
+	}
+	else {
+		i++;
+		j--;
+	}
+	fun(y, i, j);
+}
+
+int main() {
+	cin >> n;
+	fun(n, 1, 1);
+	int i = 1;
+	while (true) {
+		for (int j = 1; j <= 4; j++) {
+			if (!sum[i][1])return 0;
+			if (sum[i][j])cout << i << s[j] << sum[i][j] << " ";
+		}
+		i++;
+		cout << endl;
+	}
+	return 0;
+}
