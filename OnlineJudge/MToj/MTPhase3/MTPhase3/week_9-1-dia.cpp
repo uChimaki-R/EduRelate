a0973504493ca4486638866f861ff2819dedb167
Crestimes <1259864733@qq.com>
2024-03-31 13:45:22 +0800

    第九周模拟题组钻石难度

---
+++ OnlineJudge/MToj/MTPhase3/MTPhase3/week_9-1-dia.cpp
@@ -0,0 +1,44 @@
+﻿#include<iostream>
+#include<queue>
+#include<vector>
+using namespace std;
+
+const int N = 1007;
+
+int n, ans = 0;
+vector<queue<int>>players;
+
+int main() {
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		queue<int>qu;
+		int t;
+		for (int j = 1; j < n; j++) {
+			cin >> t;
+			qu.push(t);
+		}
+		players.push_back(qu);
+	}
+	while (true) {
+		bool goon = false;
+		bool vis[N] = { false };
+		for (int i = 0; i < players.size(); i++) {
+			if (vis[i + 1] || players[i].empty())continue;
+			int front = players[i].front();
+			if (vis[front] || players[front - 1].empty() || players[front - 1].front() != i + 1)continue;
+			goon = true;
+			vis[i + 1] = vis[front] = true;
+			players[i].pop();
+			players[front - 1].pop();
+		}
+		if (!goon)break;
+		ans++;
+	}
+	bool legal = true;
+	for (int i = 0; i < players.size(); i++) {
+		if (!players[i].empty())legal = false;
+	}
+	if (!legal)cout << -1;
+	else cout << ans;
+	return 0;
+}
---
+++ OnlineJudge/MToj/MTPhase3/MTPhase3/week_9-2-dia.cpp
@@ -0,0 +1,39 @@
+﻿#include<iostream>
+using namespace std;
+
+const int N = 1000007;
+int addn[N], arr[N];
+int n, m;
+
+int main() {
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++) {
+		cin >> arr[i];
+	}
+	while (m--) {
+		int opr;
+		cin >> opr;
+		if (opr == 1) {
+			int x, y;
+			cin >> x >> y;
+			addn[x] += y;
+		}
+		else {
+			int p;
+			cin >> p;
+			int ans = arr[p];
+			for (int i = 1; i <= p / i; i++) {  // sqrt
+				if (p % i == 0) {
+					if (i * i == p) {
+						ans += addn[i];
+					}
+					else {
+						ans += (addn[i] + addn[p / i]);
+					}
+				}
+			}
+			cout << ans << endl;
+		}
+	}
+	return 0;
+}
