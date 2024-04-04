d47b571e2e7841fe3b32779322e1570268937ee0
Crestimes <1259864733@qq.com>
2024-04-04 18:09:01 +0800

    第十周模拟题组黄金难度

---
+++ OnlineJudge/MToj/MTPhase3/MTPhase3/week_10-1-gol.cpp
@@ -0,0 +1,30 @@
+﻿#include<iostream>
+#include<deque>
+using namespace std;
+
+int n, m, k, t;
+const int N = 60;
+deque<int>qu;
+
+int main() {
+	cin >> n >> m;
+	for (int i = 0; i < n * m; i++) {
+		cin >> t;
+		qu.push_back(t);
+	}
+	cin >> k;
+	k %= (m * n);
+	while (k--) {
+		t = qu.back();
+		qu.pop_back();
+		qu.push_front(t);
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cout << qu.front() << " ";
+			qu.pop_front();
+		}
+		cout << endl;
+	}
+	return 0;
+}
---
+++ OnlineJudge/MToj/MTPhase3/MTPhase3/week_10-3-gol.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+using namespace std;
+
+int main() {
+	int a, m;
+	cin >> a >> m;
+	for (int i = 0; i < 100000; i++) {
+		a += a % m;
+		if (a % m == 0) {
+			cout << "Yes";
+			return 0;
+		}
+	}
+	cout << "No";
+	return 0;
+}
