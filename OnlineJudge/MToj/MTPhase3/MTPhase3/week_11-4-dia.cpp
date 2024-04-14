5dc3250f18a7f40d9b9f957aa1cc8d5311ef457e
Crestimes <1259864733@qq.com>
2024-04-14 21:28:36 +0800

    第十一周二分题组钻石难度

---
+++ OnlineJudge/MToj/MTPhase3/MTPhase3/week_11-4-dia.cpp
@@ -0,0 +1,40 @@
+﻿#include<iostream>
+#include<algorithm>
+using namespace std;
+
+const int N = 100007;
+int l, n, k;
+int arr[N];
+
+bool check(int len) {
+	int cnt = 0;
+	for (int i = 1; i < n; i++) {
+		int dif = arr[i] - arr[i - 1];
+		if (dif > len) {
+			cnt += dif / len;
+			if (!(dif % len))cnt -= 1;
+		}
+	}
+	if (cnt <= k)return true;
+	else return false;
+}
+
+int main() {
+	cin >> l >> n >> k;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	sort(arr, arr + n);
+	int le = 1, ri = l;
+	int ans = 0;
+	while (le <= ri) {
+		int mi = le + (ri - le) / 2;
+		if (check(mi)) {
+			ans = mi;
+			ri = mi - 1;
+		}
+		else le = mi + 1;
+	}
+	cout << ans;
+	return 0;
+}
---
+++ OnlineJudge/MToj/MTPhase3/MTPhase3/week_11-5-dia.cpp
@@ -0,0 +1,41 @@
+﻿#include<iostream>
+#include<algorithm>
+using namespace std;
+#define int long long
+
+const int N = 60;
+int n, m;
+int arr[N];
+
+bool check(int len) {
+	int cnt = 0;
+	for (int i = 0; i < n; i++) {
+		if (len > arr[i]) {
+			cnt += len - arr[i];
+		}
+	}
+	if (cnt <= len && cnt <= m)return true;
+	else return false;
+}
+
+signed main() {
+	cin >> n >> m;
+	int le = 0, ri = 0;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+		ri = max(arr[i], ri);
+	}
+	ri += m;
+	sort(arr, arr + n);
+	int ans = 0;
+	while (le <= ri) {
+		int mi = le + (ri - le) / 2;
+		if (check(mi)) {
+			ans = mi;
+			le = mi + 1;
+		}
+		else ri = mi - 1;
+	}
+	cout << ans;
+	return 0;
+}
