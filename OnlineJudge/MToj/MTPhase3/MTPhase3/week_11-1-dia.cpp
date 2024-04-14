061e3e6fc5d65274cbedeb20635e6d0b3502cc5d
Crestimes <1259864733@qq.com>
2024-04-14 21:16:14 +0800

    第十一周模拟题组钻石难度

---
+++ OnlineJudge/MToj/MTPhase3/MTPhase3/week_11-1-dia.cpp
@@ -0,0 +1,32 @@
+﻿#include<iostream>
+#include<vector>
+#include<map>
+using namespace std;
+
+map<string, int>mp1;
+map<int, string>mp2;
+int n, m, t, a, b;
+string s1, s2;
+
+int main() {
+	cin >> n >> m >> t;
+	for (int i = 1; i <= n; i++) {
+		cin >> s1;
+		mp1[s1] = i;
+		mp2[i] = s1;
+	}
+	for (int i = 1; i <= t; i++) {
+		cin >> a >> b;
+		s1 = mp2[a];
+		s2 = mp2[b];
+		mp2[a] = s2;
+		mp2[b] = s1;
+		mp1[s1] = b;
+		mp1[s2] = a;
+	}
+	for (int i = 1; i <= m; i++) {
+		cin >> s1;
+		cout << mp1[s1] << endl;
+	}
+	return 0;
+}
