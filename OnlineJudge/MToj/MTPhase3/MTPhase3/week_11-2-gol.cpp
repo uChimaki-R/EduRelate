d762f0de19324ac010a230021f0f16244aebe4c7
Crestimes <1259864733@qq.com>
2024-04-14 20:41:09 +0800

    第十一周模拟题组黄金难度

---
+++ OnlineJudge/MToj/MTPhase3/MTPhase3/week_11-2-gol.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+#include<iomanip>
+using namespace std;
+
+const int N = 1007;
+float arr1[N], arr2[N];
+
+int main() {
+	int n, m, x, y;
+	float z;
+	cin >> n >> m;
+	for (int i = 1; i <= n; i++) {
+		cin >> arr1[i];
+	}
+	float ans = 0;
+	while (m--) {
+		cin >> x >> y >> z;
+		if (arr1[x] < z) {
+			cout << fixed << setprecision(2) << ans;
+			return 0;
+		}
+		arr1[x] -= z;
+		arr1[y] += int(z);
+		float cnt = z - int(z);
+		ans += cnt;
+		arr2[y] += cnt;
+		if (arr2[y] > 1) {
+			cout << fixed << setprecision(2) << ans;
+			return 0;
+		}
+	}
+	cout << fixed << setprecision(2) << ans;
+	return 0;
+}
