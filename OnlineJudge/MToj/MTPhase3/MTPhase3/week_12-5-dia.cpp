0db3d78543340762dbf3aedbb6415f4c47ff2a9e
Crestimes <1259864733@qq.com>
2024-04-21 15:14:24 +0800

    歌词中找单词：二分

---
+++ OnlineJudge/MToj/MTPhase3/MTPhase3/week_12-5-dia.cpp
@@ -0,0 +1,33 @@
+﻿#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+const int N = 100007;
+string words[N], s;
+vector<int> w[30];
+
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)cin >> words[i];
+	cin >> s;
+	for (int i = 0; i < s.length(); i++) {
+		w[s[i] - 'a'].push_back(i);
+	}
+	for (int i = 0; i < n; i++) {
+		int p = -1;
+		bool flag = true;
+		for (int j = 0; j < words[i].length(); j++) {
+			auto it = lower_bound(w[words[i][j] - 'a'].begin(), w[words[i][j] - 'a'].end(), p);
+			if (it == w[words[i][j] - 'a'].end()) {
+				flag = false;
+				break;
+			}
+			p = w[words[i][j] - 'a'][distance(w[words[i][j] - 'a'].begin(), it)];
+		}
+		if (flag)cout << "YES" << endl;
+		else cout << "NO" << endl;
+	}
+	return 0;
+}
