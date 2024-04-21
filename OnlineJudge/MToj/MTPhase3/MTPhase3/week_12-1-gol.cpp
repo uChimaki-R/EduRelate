a1f1eeb42ddf63b14aa05daefaf58a7e6b649504
Crestimes <1259864733@qq.com>
2024-04-21 14:22:30 +0800

    第十二周递归题组黄金难度

---
+++ OnlineJudge/MToj/MTPhase3/MTPhase3/week_12-1-gol.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+using namespace std;
+#define int long long
+
+const int N = 90;
+string a0 = "IAKIOI";
+string a1 = "WHENWILLSCORLLOFTAIWUCOMEOUT!!!";
+int len[N];
+
+char find(int n, int c) {
+	if (n == 0)return a0[c - 1];
+	else if (n == 1)return a1[c - 1];
+	if (c <= len[n - 2])return find(n - 2, c);
+	else return find(n - 1, c - len[n - 2]);
+}
+
+signed main() {
+	int n, c;
+	cin >> n >> c;
+	len[0] = a0.length();
+	len[1] = a1.length();
+	for (int i = 2; i < N; i++) {
+		len[i] = len[i - 1] + len[i - 2];
+	}
+	cout << find(n, c);
+	return 0;
+}
---
+++ OnlineJudge/MToj/MTPhase3/MTPhase3/week_12-6-gol.cpp
@@ -0,0 +1,15 @@
+﻿#include<iostream>
+using namespace std;
+#define int long long
+
+int a[30] = { 0, 0, 1 };
+
+signed main() {
+	int n;
+	cin >> n;
+	for (int i = 3; i <= n; i++) {
+		a[i] = (i - 1) * (a[i - 1] + a[i - 2]);
+	}
+	cout << a[n];
+	return 0;
+}
