94926984ac07b4547887e19bf204b2cdde78bb37
Crestimes <1259864733@qq.com>
2024-04-21 14:50:44 +0800

    砍玉米：贪心

---
+++ OnlineJudge/MToj/MTPhase3/MTPhase3/week_12-4-dia.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+using namespace std;
+#define int long long
+
+const int N = 100007;
+int x[N], h[N];
+
+signed main() {
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)cin >> x[i] >> h[i];
+	x[0] = -2e9 + 7;
+	x[n] = 2e9 + 7;
+	int ans = 1;
+	for (int i = 1; i < n; i++) {
+		if (x[i] - x[i - 1] > h[i])ans++;
+		else if (x[i + 1] - x[i] > h[i]) {
+			ans++;
+			x[i] = x[i] + h[i];
+		}
+	}
+	cout << ans;
+	return 0;
+}
