676159d763f92254fa7ee547d297c6fcf2be670e
Crestimes <1259864733@qq.com>
2024-09-02 21:40:32 +0800

    medium: 哈希表

---
+++ OnlineJudge/LeetCode/leetcode/src/main/java/com/rpocky/leetcode/editor/cn/P1TwoSum.java
@@ -0,0 +1,81 @@
+//给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target 的那 两个 整数，并返回它们的数组下标。 
+//
+// 你可以假设每种输入只会对应一个答案，并且你不能使用两次相同的元素。 
+//
+// 你可以按任意顺序返回答案。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：nums = [2,7,11,15], target = 9
+//输出：[0,1]
+//解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：nums = [3,2,4], target = 6
+//输出：[1,2]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：nums = [3,3], target = 6
+//输出：[0,1]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 2 <= nums.length <= 10⁴ 
+// -10⁹ <= nums[i] <= 10⁹ 
+// -10⁹ <= target <= 10⁹ 
+// 只会存在一个有效答案 
+// 
+//
+// 
+//
+// 进阶：你可以想出一个时间复杂度小于 O(n²) 的算法吗？ 
+//
+// Related Topics 数组 哈希表 👍 18846 👎 0
+
+package com.rpocky.leetcode.editor.cn;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+//Java：两数之和
+public class P1TwoSum {
+    public static void main(String[] args) {
+        // TO TEST
+        Solution solution = new P1TwoSum().new Solution();
+        System.out.println(Arrays.toString(solution.twoSum(new int[]{2, 7, 11, 15}, 9)));
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int[] twoSum(int[] nums, int target) {
+            // 键值对 key记录value下标的数字比target小多少
+            HashMap<Integer, Integer> map = new HashMap<>();
+            for (int i = 0; i < nums.length; i++) {
+                int diff = target - nums[i];
+                // 判断是否有刚好加上nums[i]就等于target的数
+                if (map.containsKey(nums[i])) {
+                    // 有则获取其下标，和自己的下标一起返回
+                    return new int[]{map.get(nums[i]), i};
+                }
+                // 没有则把自己需要加上的数值和自己的下标放入键值对中，待后续其他数字找自己
+                map.put(diff, i);
+            }
+            return null;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
